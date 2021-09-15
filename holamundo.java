d47b546184f743a06899d372d39160dd0d3214f0
¨AlfredoHernandez45¨ <l18390045@chetumal.tecnm.mx>
2021-09-14 22:16:26 -0500

    Seguimiento de practica

--- holamundo.java
+++ holamundo.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class holamundo {
     public static void main (String [] args){
         System.out.println("\nHola mundo\n");
-        //hola puto 
+        //hola pureba
     }
 
 }
